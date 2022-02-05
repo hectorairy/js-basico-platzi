944149b8c6abe13cd6a3882d902c4260e87aac72
Héctor Airy <hola@hectorairy.com>
2022-02-04 23:19:21 -0600

    Conditional if

---
+++ if.js
@@ -0,0 +1,19 @@
+// Condicionales
+
+// If
+if (true) {
+  console.log("Verdadero");
+} else {
+  console.log("Falso");
+}
+
+var edad = 18;
+if (edad >= 18) {
+  console.log("Puedes votar");
+} else {
+  console.log("Aún no puedes votar :( ");
+}
+
+// Operador ternario
+
+condition ? true : false;
