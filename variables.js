ccdd9f3ee34a5a6a9e3a7c78c419167a67e9d389
Héctor Airy <hola@hectorairy.com>
2022-02-04 21:15:44 -0600

    Variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+// Esta es una variable inicializada
+var nombre = "Héctor";
+
+// Declarar una variable
+var edad;
+
+// Inicializar la variable (darle un valor)
+edad = 30;
+
+// El signo = es igual a operador de asignación
+
+// Podemos generar variables con los valores primitivos y ni primitivos
+// En la siguiente variable asignaremos un tipo de dato array
+var elementos = ["Computadora", "Celular"];
+
+// de igual forma para el tipo objeto
+var persona = {
+  nombre: "Héctor",
+  edad: 24,
+};
