5fe5efdc360d3c4198ac6771899acb8339320fa8
Héctor Airy <hola@hectorairy.com>
2022-02-04 23:35:23 -0600

    Switch

---
+++ switch.js
@@ -0,0 +1,11 @@
+var numero = null;
+switch (numero) {
+  case 1:
+    console.log("Soy un uno!");
+    break;
+  case 2:
+    console.log("Soy un dos!!!");
+  default:
+    console.log("No soy nada :( ");
+    break;
+}
